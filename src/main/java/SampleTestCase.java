a64dd363b8b7961f78aaa7026dc14f8c05a4612d
aromanchenkonn <a.romanchenko.nn@gmail.com>
2017-09-21 22:44:26 +0300

    add new project to test restapi service

---
+++ src/main/java/SampleTestCase.java
@@ -0,0 +1,30 @@
+import controllers.UserController;
+import models.User;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.web.client.RestTemplate;
+
+import static org.junit.Assert.*;
+
+public class SampleTestCase {
+
+    private UserController userController;
+
+    @Before
+    public void setUp() {
+        userController = new UserController();
+    }
+
+    @Test
+    public void testUserInfoResponse() {
+        User remoteUser = userController.getUserByUsername("aromanchenkonn");
+
+        assertTrue(remoteUser.getHtml_url().contains("https://github.com/aromanchenkonn"));
+    }
+
+    @Test
+    public void testFollowers() {
+        User[] followers = userController.getFollowersByUser("aromanchenkonn");
+        assertTrue(followers.length == 0);
+    }
+}
